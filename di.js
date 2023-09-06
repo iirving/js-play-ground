380ff6a15bf23bb95d010e867412f4ae47481a8c
Ian Irving <ian@falsepositives.com>
2023-09-05 22:54:10 -0400

    move dropElements tests into jest

--- di.js
+++ di.js
@@ -13,7 +13,6 @@ function dropElements(arr, func) {
     pos = i;
     let element = arr[i];
     let result = func(element);
-    console.log("function is true", "e=", element, result, pos);
     if (result) {
       satisfied = true;
       break;
@@ -27,44 +26,3 @@ function dropElements(arr, func) {
 module.exports = {
   dropElements,
 };
-
-// console.log(
-//   dropElements([1, 2, 3, 4], function (n) {
-//     return n >= 3;
-//   })
-// );
-
-// console.log(
-//   dropElements([0, 1, 0, 1], function (n) {
-//     return n === 1;
-//   })
-// );
-
-// console.log(
-//   dropElements([1, 2, 3, 7, 4], function (n) {
-//     return n > 3;
-//   })
-// ); // should return [7, 4].
-
-// console.assert(
-//   dropElements([1, 2, 3, 4], function (n) {
-//     return n >= 3;
-//   }),
-//   [3, 4],
-//   "should return [3, 4]."
-// );
-
-// console.log(
-//   dropElements([0, 1, 0, 1], function (n) {
-//     return n === 1;
-//   })
-// ); //) should return [1, 0, 1].
-
-// console.log(:dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
-console.log(
-  dropElements([1, 2, 3, 4], function (n) {
-    return n > 5;
-  })
-); // should return [].
-// console.log(:dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
-// console.log(:dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
--- di.test.js
+++ di.test.js
@@ -1,17 +1,4 @@
-// Drop it
-// Given the array arr, iterate through and remove each element starting from the first element
-// (the 0 index) until the function func returns true when the iterated element is passed through it.
-
-// Then return the rest of the array once the condition is satisfied, otherwise,
-// arr should be returned as an empty array.
-
 const { dropElements } = require("./di");
-
-// console.log(
-//   dropElements([1, 2, 3, 4], function (n) {
-//     return n >= 3;
-//   })
-// );
 
 describe("test dropElements function ", () => {
   test("should return [7, 4].", () => {
@@ -21,39 +8,44 @@ describe("test dropElements function ", () => {
 
     expect(result).toEqual([7, 4]);
   });
+
+  test("should return [1, 0, 1].", () => {
+    let result = dropElements([0, 1, 0, 1], function (n) {
+      return n === 1;
+    });
+
+    expect(result).toEqual([1, 0, 1]);
+  });
+
+  test("should return [3, 4].", () => {
+    let result = dropElements([1, 2, 3, 4], function (n) {
+      return n >= 3;
+    });
+
+    expect(result).toEqual([3, 4]);
+  });
+
+  test("should return [].", () => {
+    let result = dropElements([1, 2, 3, 4], function (n) {
+      return n > 5;
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  test("should return [7, 4].", () => {
+    let result = dropElements([1, 2, 3, 7, 4], function (n) {
+      return n > 3;
+    });
+
+    expect(result).toEqual([7, 4]);
+  });
+
+  test("should return [3, 9, 2].", () => {
+    let result = dropElements([1, 2, 3, 9, 2], function (n) {
+      return n > 2;
+    });
+
+    expect(result).toEqual([3, 9, 2]);
+  });
 });
-
-// console.log(
-//   dropElements([0, 1, 0, 1], function (n) {
-//     return n === 1;
-//   })
-// );
-
-// console.log(
-//   dropElements([1, 2, 3, 7, 4], function (n) {
-//     return n > 3;
-//   })
-// ); // should return [7, 4].
-
-// console.assert(
-//   dropElements([1, 2, 3, 4], function (n) {
-//     return n >= 3;
-//   }),
-//   [3, 4],
-//   "should return [3, 4]."
-// );
-
-// console.log(
-//   dropElements([0, 1, 0, 1], function (n) {
-//     return n === 1;
-//   })
-// ); //) should return [1, 0, 1].
-
-// console.log(:dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
-console.log(
-  dropElements([1, 2, 3, 4], function (n) {
-    return n > 5;
-  })
-); // should return [].
-// console.log(:dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
-// console.log(:dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
