0f45244511d23edd9a5946b9dee82a292d223038
Ian Irving <ian@falsepositives.com>
2023-10-25 14:50:38 -0400

    add and cleanup JSDOC  annotations

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -13,8 +13,14 @@
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const ROTATE_VALUE = 13;
 
+/**
+ * takes a ROT13 encoded string as input and returns a decoded string.
+ * @param {String} str - a ROT13 encoded string string to be decoded
+ * @returns {String} a decoded string
+ */
 export default function rot13(str) {
   return str
+    .toUpperCase()
     .split("")
     .map((element, x) => {
       return _isInAlphabet(element) ? _rotateElement(element) : element;
@@ -26,8 +32,8 @@ export default function rot13(str) {
  * get the location of the element in the alphabetic list
  * and use that to calucation the rotated location,
  * and then return the new alphabetic element based on the rotated location
- * @param {String} element to rotate
- * @returns {String} new reotoated element
+ * @param {String} element - a character to rotate
+ * @returns {String} new rotated element
  */
 function _rotateElement(element) {
   let location = _locationInAlphabet(element);
@@ -38,14 +44,14 @@ function _rotateElement(element) {
 /**
  * where from A to Z is the character located with A being 0 and Z begining 25 (25-1)
  * @param {String} character  - a single uppercase character
- * @returns {Number} location
+ * @returns {Number} location in ALPHABET string from 0 to length - 1; -1 if not in string
  */
 function _locationInAlphabet(character) {
   return ALPHABET.indexOf(character);
 }
 /**
  * is the character in the Alphabet
- * @param {String} character @param {string} character  - a single uppercase character
+ * @param {String} character  - a single uppercase character
  * @returns {Boolean} true if location in Alphabet is greater than or equal to 0
  */
 function _isInAlphabet(character) {
@@ -56,7 +62,7 @@ function _isInAlphabet(character) {
  * if Rotoation would bring it past the end of the Alphabet
  * then start at the beging of the Alphabet
  * otherwise shift the location by ROTATE_VALUE
- * @param {Number} location
+ * @param {Number} location - a location to be ROT13 adjuested
  * @returns {Number} a new single character value location
  */
 function _newRotaedLocation(location) {
